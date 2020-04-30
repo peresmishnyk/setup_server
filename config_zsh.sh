d77085ec0932b7e909d27c5328529caa9de1d59a
Дмитрий Мичкире <michkire@gmail.com>
2020-05-01 00:33:17 +0300

    fix

--- config_zsh.sh
+++ config_zsh.sh
@@ -1,4 +1,3 @@
 $(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
-cp -f zsh-config/* ~
 zsh
---
+++ login_entry_point.sh
@@ -0,0 +1,10 @@
+git pull
+
+</dev/tty read -p "Install? [y,n]" -n 1 doit
+case $doit in
+  y|Y) echo yes ;;
+  n|N) echo no ;;
+  *) echo dont know ;;
+esac
+#./install.sh
+cd ~
