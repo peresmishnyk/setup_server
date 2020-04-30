7f36d876261c37cef4768ec95226516319774c4e
Дмитрий Мичкире <michkire@gmail.com>
2020-05-01 01:33:35 +0300

    fix

--- setup.sh
+++
@@ -1,6 +0,0 @@
-if [ -d "~/.git" ]; then
-  echo "Skip init repo"
-else
-  echo "Init repo"
-  git clone --no-checkout git@github.com:peresmishnyk/setup_server.git ~/setup && mv ~/setup/.git ~/.git && rm -R ~/setup && git reset --hard
-fi
