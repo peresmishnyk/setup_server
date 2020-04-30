e4373ccc0d33a9971cde072aa5a448c90dd26819
Дмитрий Мичкире <michkire@gmail.com>
2020-05-01 01:51:01 +0300

    fix

--- script/start.sh
+++ script/start.sh
@@ -3,7 +3,7 @@ SCRIPT=$(readlink -f "$0")
 # Absolute path this script is in, thus /home/user/bin
 SCRIPTPATH=$(dirname "$SCRIPT")
 
-cd $(dirname "$SCRIPTPATH")/.. && git pull && cd $(dirname "$SCRIPTPATH")
+cd $(dirname "$SCRIPTPATH")/.. && pwd && git pull && cd $(dirname "$SCRIPTPATH")
 
 read -p "Install ZSH? [y,n]" -n 1 doit
 case $doit in
