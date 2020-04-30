a90905976ffc4e8d78a463f544e185cb577dc8e3
Дмитрий Мичкире <michkire@gmail.com>
2020-05-01 02:59:23 +0300

    fix

--- script/add_developer_user.sh
+++ script/add_developer_user.sh
@@ -1,3 +1,3 @@
-adduser developer --shell `which zsh` --no-create-home --system --group www-data
+adduser developer --shell `which zsh` --no-create-home --system --ingroup www-data
 usermod -aG sudo developer
 echo "developer	ALL=(ALL:ALL) NOPASSWD:ALL" > /etc/sudoers.d/developer
