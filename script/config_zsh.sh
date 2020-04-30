dee3ddcabb43e2c7e02cfb11d8c8ce913905ad3b
Дмитрий Мичкире <michkire@gmail.com>
2020-05-01 02:01:02 +0300

    fix

--- script/config_zsh.sh
+++ script/config_zsh.sh
@@ -1,3 +1,5 @@
 $(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
-zsh
+cp ../config/.p10k.zsh ~
+cp ../config/.zshrc ~
+source ~/.zshrc
