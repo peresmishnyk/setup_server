9efc7e6136dcea8c15b70a6fe3d0345ffa3977cc
Дмитрий Мичкире <michkire@gmail.com>
2020-05-01 03:18:57 +0300

    fix

--- script/add_developer_user.sh
+++ script/add_developer_user.sh
@@ -1,12 +1,6 @@
+userdel -r developer
 adduser developer --shell `which zsh` --system --ingroup www-data
 usermod -aG sudo developer
 echo "developer	ALL=(ALL:ALL) NOPASSWD:ALL" > /etc/sudoers.d/developer
 
-su - developer "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-su - developer "git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k"
-#cp ../config/.p10k.zsh ~
-#cp ../config/.zshrc ~
-
-#cp config_zsh.sh /home/developer
-#chown /home/developer/config_zsh
-#sudo
+su - developer -c "cd ~ && git clone git@github.com:peresmishnyk/setup_server.git ~/setup && cd ~/setup/script && ./config_zsh"
