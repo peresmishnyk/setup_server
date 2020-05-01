b98033b438ca5e241cbebebeb4f654d759add900
Дмитрий Мичкире <michkire@gmail.com>
2020-05-01 06:02:18 +0300

    fix

--- script/install_mysql.sh
+++ script/install_mysql.sh
@@ -1,5 +1,6 @@
 apt -y install gnupg2
 wget https://repo.percona.com/apt/percona-release_latest.$(lsb_release -sc)_all.deb
 dpkg -i percona-release_latest.$(lsb_release -sc)_all.deb
+percona-release setup ps80
 apt -y update
-apt -y install percona-server-server-8.0
+apt -y install percona-server-server
