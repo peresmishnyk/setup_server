9991776d56ee5cbad4aaf6dbe11e844a7ded4819
Дмитрий Мичкире <michkire@gmail.com>
2020-05-01 06:17:25 +0300

    fix

---
+++ script/setup_db_user.sh
@@ -0,0 +1,10 @@
+newUser='testuser'
+newDbPassword='testpwd'
+newDb='testdb'
+#host=localhost
+host='%'
+
+commands="CREATE DATABASE \`${newDb}\`;CREATE USER '${newUser}'@'${host}' IDENTIFIED BY '${newDbPassword}';GRANT USAGE ON *.* TO '${newUser}'@'${host}' IDENTIFIED BY '${newDbPassword}';GRANT ALL privileges ON \`${newDb}\`.*
+TO '${newUser}'@'${host}';FLUSH PRIVILEGES;"
+
+echo "${commands}" | /usr/bin/mysql -u root -p
