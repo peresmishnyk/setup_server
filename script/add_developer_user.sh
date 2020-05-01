5b0edeb381ccf0ec1ae14a58229c2c7b70678ba7
Дмитрий Мичкире <michkire@gmail.com>
2020-05-01 04:14:31 +0300

    fix

--- script/add_developer_user.sh
+++ script/add_developer_user.sh
@@ -1,11 +1,12 @@
 userdel -r developer
 adduser developer --shell `which zsh` --system --ingroup www-data
 usermod -aG sudo developer
-echo "developer	ALL=(ALL:ALL) NOPASSWD:ALL" > /etc/sudoers.d/developer
 
-
+clear
 export PASSWD="linuxpassword"
 echo -e "$PASSWD\n$PASSWD" | passwd developer
+
+echo "developer	ALL=(ALL:ALL) NOPASSWD:ALL" > /etc/sudoers.d/developer
 
 mkdir -p /home/developer/.ssh
 cp ~/.ssh/* /home/developer/.ssh
