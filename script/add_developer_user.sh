9b6c6dc00669687b43fd70499b46f0e8e6fc6934
Дмитрий Мичкире <michkire@gmail.com>
2020-05-01 03:22:09 +0300

    fix

--- script/add_developer_user.sh
+++ script/add_developer_user.sh
@@ -3,4 +3,7 @@ adduser developer --shell `which zsh` --system --ingroup www-data
 usermod -aG sudo developer
 echo "developer	ALL=(ALL:ALL) NOPASSWD:ALL" > /etc/sudoers.d/developer
 
+cp ~/.ssh/id_rsa /home/developer/.ssh/id_rsa
+cp ~/.ssh/id_rsa.pub /home/developer/.ssh/id_rsa.pub
+chown -R developer:www-data /home/developer/.ssh
 su - developer -c "cd ~ && git clone git@github.com:peresmishnyk/setup_server.git ~/setup && cd ~/setup/script && ./config_zsh"
