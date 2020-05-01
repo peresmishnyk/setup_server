4c75254190df568b342985c1f5b1104fcb533729
Дмитрий Мичкире <michkire@gmail.com>
2020-05-01 06:05:11 +0300

    fix

--- script/install_mysql.sh
+++ script/install_mysql.sh
@@ -3,4 +3,4 @@ wget https://repo.percona.com/apt/percona-release_latest.$(lsb_release -sc)_all.
 dpkg -i percona-release_latest.$(lsb_release -sc)_all.deb
 percona-release setup ps80
 apt -y update
-apt -y install percona-server-server
+DEBIAN_FRONTEND=noninteractive apt-get -y install percona-server-server
