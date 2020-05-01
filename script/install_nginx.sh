04bf7fd3dd46dcedaad0bd9ea184d577cb7de737
Дмитрий Мичкире <michkire@gmail.com>
2020-05-01 05:46:32 +0300

    fix

--- script/install_nginx.sh
+++ script/install_nginx.sh
@@ -1,6 +1,14 @@
 apt -y install nginx
 
 rm /etc/nginx/sites-enabled/default
+rm -R /var/www/html
+
+chown -R developer:www-data /var/www
+chmod g+s /var/www
+
 cp $SCRIPTPATH/../nginx/default /etc/nginx/sites-enabled/default
+
+mkdir -p /var/www/default
+cp $SCRIPTPATH/../nginx/index.php /var/www/default/index.php
 
 service nginx reload
