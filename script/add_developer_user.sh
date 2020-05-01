82ab84a34eebd252159f2a8274a88c3b0c1d6816
Дмитрий Мичкире <michkire@gmail.com>
2020-05-01 04:06:46 +0300

    fix

--- script/add_developer_user.sh
+++ script/add_developer_user.sh
@@ -3,14 +3,12 @@ adduser developer --shell `which zsh` --system --ingroup www-data
 usermod -aG sudo developer
 echo "developer	ALL=(ALL:ALL) NOPASSWD:ALL" > /etc/sudoers.d/developer
 
-DEVELOPER_PASSWD=`head /dev/urandom | tr -dc A-Za-z0-9 | head -c 13 ; echo ''`
+PASSWD=`head /dev/urandom | tr -dc A-Za-z0-9 | head -c 13 ; echo ''`
 
-DEVELOPER_PASSWD='123456789'
-
-echo -e "$DEVELOPER_PASSWD\n$DEVELOPER_PASSWD" | passwd developer
+echo -e "$PASSWD\n$PASSWD" | passwd developer
 
 mkdir -p /home/developer/.ssh
 cp ~/.ssh/* /home/developer/.ssh
 chown -R developer:www-data /home/developer/.ssh
 su - developer -c "cd ~ && git clone git@github.com:peresmishnyk/setup_server.git ~/setup && cd ~/setup/script && ./config_zsh.sh"
-echo "developer password is: $DEVELOPER_PASSWD"
+echo "developer password is: '$PASSWD'"
