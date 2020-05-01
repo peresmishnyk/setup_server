1fce5c2cde286689662fc4252e7c674697bffca9
Дмитрий Мичкире <michkire@gmail.com>
2020-05-01 05:05:16 +0300

    fix

--- script/install_php.sh
+++ script/install_php.sh
@@ -1,5 +1,5 @@
 apt -y install software-properties-common
 echo | add-apt-repository ppa:ondrej/php
-echo "\n" | apt -y update
+apt -y update
 PHP="php7.3"
 apt -y install zip openssl $PHP-fpm $PHP-common $PHP-curl $PHP-json $PHP-mbstring $PHP-mysql $PHP-xml $PHP-zip
