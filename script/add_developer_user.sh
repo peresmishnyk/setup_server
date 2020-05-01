d21c5f0aea600ee01738871453d0fe34d50ac91e
Дмитрий Мичкире <michkire@gmail.com>
2020-05-01 03:30:24 +0300

    fix

--- script/add_developer_user.sh
+++ script/add_developer_user.sh
@@ -4,7 +4,6 @@ usermod -aG sudo developer
 echo "developer	ALL=(ALL:ALL) NOPASSWD:ALL" > /etc/sudoers.d/developer
 
 mkdir -p /home/developer/.ssh
-cp ~/.ssh/id_rsa /home/developer/.ssh/id_rsa
-cp ~/.ssh/id_rsa.pub /home/developer/.ssh/id_rsa.pub
+cp ~/.ssh/* /home/developer/.ssh
 chown -R developer:www-data /home/developer/.ssh
 su - developer -c "cd ~ && git clone git@github.com:peresmishnyk/setup_server.git ~/setup && cd ~/setup/script && ./config_zsh"
