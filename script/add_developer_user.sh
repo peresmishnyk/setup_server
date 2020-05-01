0c603664e855e57667b690eb11d7843b3a674c8d
Дмитрий Мичкире <michkire@gmail.com>
2020-05-01 04:28:46 +0300

    fix

--- script/add_developer_user.sh
+++ script/add_developer_user.sh
@@ -10,4 +10,4 @@ mkdir -p /home/developer/.ssh
 cp ~/.ssh/* /home/developer/.ssh
 chown -R developer:www-data /home/developer/.ssh
 su - developer -c "cd ~ && git clone git@github.com:peresmishnyk/setup_server.git ~/setup && cd ~/setup/script && ./config_zsh.sh"
-echo "developer password is: '$PASSWD'"
+echo "\nDeveloper password is: '$PASSWD'"
