eb6de57858b573a6d8885de788609e741164eb50
Дмитрий Мичкире <michkire@gmail.com>
2020-05-01 06:45:28 +0300

    fix

--- script/install_mysql.sh
+++ script/install_mysql.sh
@@ -4,3 +4,7 @@ dpkg -i percona-release_latest.$(lsb_release -sc)_all.deb
 percona-release setup ps80
 apt -y update
 DEBIAN_FRONTEND=noninteractive apt-get -y install percona-server-server
+
+# fix password
+echo "[mysqld]\ndefault-authentication-plugin=mysql_native_password" > /etc/my.cnf
+service mysql restart
--- script/setup_db_user.sh
+++ script/setup_db_user.sh
@@ -1,12 +1,14 @@
-newUser='test'
-newDbPassword='test'
-newDb='test'
+newUser='test3'
+newDbPassword='test3'
+newDb='test3'
 #host=localhost
 host='%'
 
-commands="CREATE DATABASE \`${newDb}\`;CREATE USER '${newUser}'@'${host}' IDENTIFIED BY '${newDbPassword}';GRANT USAGE ON \*.\* TO '${newUser}'@'${host}' IDENTIFIED BY '${newDbPassword}';GRANT ALL privileges ON \`${newDb}\`.*
+commands="CREATE DATABASE \`${newDb}\`;CREATE USER '${newUser}'@'${host}' IDENTIFIED BY '${newDbPassword}';GRANT USAGE ON *.* TO '${newUser}'@'${host}' IDENTIFIED BY '${newDbPassword}';GRANT ALL privileges ON \`${newDb}\`.*
 TO '${newUser}'@'${host}';FLUSH PRIVILEGES;"
 
-#echo "${commands}" | /usr/bin/mysql -u root
+echo ${commands} | /usr/bin/mysql -u root
 
-echo $commands
+#echo $commands
+
+#CREATE DATABASE `test2`;CREATE USER 'test2'@'%' IDENTIFIED BY 'test2';GRANT USAGE ON *.* TO 'test2'@'%' IDENTIFIED BY 'test2';GRANT ALL privileges ON `test2`.* TO 'test2'@'%';FLUSH PRIVILEGES;
