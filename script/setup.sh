d8eb75204ca4349ed50a8508c52e1c6572d820da
Дмитрий Мичкире <michkire@gmail.com>
2020-05-01 04:33:03 +0300

    fix

--- script/setup.sh
+++ script/setup.sh
@@ -3,10 +3,10 @@ echo "~/setup/script/start.sh" >> ~/.bashrc
 # Absolute path this script is in, thus /home/user/bin
 SCRIPTPATH=$(dirname $0)
 
-cd $SCRIPTPATH/.. && pwd && git pull && cd $SCRIPTPATH
+cd $SCRIPTPATH/.. && pwd && git pull
 
-./install_zsh.sh
-./install_nginx.sh
-./install_php.sh
-./add_developer.sh
+cd $SCRIPTPATH && ./install_zsh.sh
+cd $SCRIPTPATH && ./install_nginx.sh
+cd $SCRIPTPATH && ./install_php.sh
+cd $SCRIPTPATH && ./add_developer.sh
 #echo "cd ~/setup && git pull && cp ./config/.zshrc ~/.zshrc && source ~/.zshrc" > ~/.ssh/rc
