db0fd446f90217d26164355c3518e0d2b67d1d34
Дмитрий Мичкире <michkire@gmail.com>
2020-05-01 04:58:46 +0300

    fix

--- script/setup.sh
+++ script/setup.sh
@@ -5,8 +5,8 @@ SCRIPTPATH=$(exec 2>/dev/null;cd -- $(dirname "$0"); unset PWD; /usr/bin/pwd || 
 
 cd $SCRIPTPATH && git pull
 
-cd $SCRIPTPATH && ./install_zsh.sh
-cd $SCRIPTPATH && ./install_nginx.sh
-cd $SCRIPTPATH && ./install_php.sh
-cd $SCRIPTPATH && ./add_developer.sh
+./install_zsh.sh
+./install_nginx.sh
+./install_php.sh
+./add_developer_user.sh
 #echo "cd ~/setup && git pull && cp ./config/.zshrc ~/.zshrc && source ~/.zshrc" > ~/.ssh/rc
